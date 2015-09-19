afc6d8229cdfc7d9c4d579773c4c33461a6ee3a3
rhenvar <garrickkrol@hotmail.com>
2015-09-19 13:13:07 -0700

    Cleaning up :)

--- Main.java
+++ Main.java
@@ -1,47 +1,48 @@
 import java.util.*;
 
 public class Main {
-   public static void main(String[] args) {
-      TreeSet<Integer> treeTest = new TreeSet<Integer>();
+    public static void main(String[] args) {
+	TreeSet<Integer> treeTest = new TreeSet<Integer>();
 
-      Random r = new Random();
-      int n = 10000;
-      long startTimeAdd = System.nanoTime();
-      for (int i = 0; i < n; i++) {
-         treeTest.add(r.nextInt(n));
-      }
-      long endTimeAdd = (System.nanoTime() - startTimeAdd);
+	Random r = new Random();
+	int n = 10000;
+	long startTimeAdd = System.nanoTime();
+	for (int i = 0; i < n; i++) {
+	    treeTest.add(r.nextInt(n));
+	}
+	long endTimeAdd = (System.nanoTime() - startTimeAdd);
 
-      long startTimeIterate = System.nanoTime();
+	long startTimeIterate = System.nanoTime();
 
-      for (Integer i : treeTest) {
-         System.out.println(i);
-      }
+	for (Integer i : treeTest) {
+	    System.out.println(i);
+	}
 
-      long endTimeIterate = (System.nanoTime() - startTimeIterate);
+	long endTimeIterate = (System.nanoTime() - startTimeIterate);
 
-      System.out.println("Time to add " + n + " random items: " + endTimeAdd / 1000000 + "ms");
-      System.out.println("Time to iterate over list: " + endTimeIterate / 1000000 + "ms");
+	System.out.println("Time to add " + n + " random items: " + endTimeAdd / 1000000 + "ms");
+	System.out.println("Time to iterate over list: " + endTimeIterate / 1000000 + "ms");
 
-      int testContains = r.nextInt(n);
-      long startTimeContains = System.nanoTime();
+	int testContains = r.nextInt(n);
+	long startTimeContains = System.nanoTime();
 
-      boolean isPresent = treeTest.contains(testContains);
+	boolean isPresent = treeTest.contains(testContains);
 
-      long endTimeContains = (System.nanoTime() - startTimeContains);
+	long endTimeContains = (System.nanoTime() - startTimeContains);
 
-      System.out.println("Time to check if " + testContains + " is in the set " + endTimeContains / 1000000 + "ms " + isPresent);
-      System.out.println(treeTest);
-      
-      Iterator<Integer> t = treeTest.iterator();
-      
-      // remove all evens
-      while (t.hasNext()) {
-    	  if (t.next() % 2 == 0) {
-    		  t.remove();
-    	  }
-      }
-      
-      System.out.println(treeTest);
-   }
+	System.out.println(
+		"Time to check if " + testContains + " is in the set " + endTimeContains / 1000000 + "ms " + isPresent);
+	System.out.println(treeTest);
+
+	Iterator<Integer> t = treeTest.iterator();
+
+	// remove all evens
+	while (t.hasNext()) {
+	    if (t.next() % 2 == 0) {
+		t.remove();
+	    }
+	}
+
+	System.out.println(treeTest);
+    }
 }
--- TreeSet.java
+++ TreeSet.java
@@ -8,196 +8,195 @@ import java.util.Stack;
  */
 
 public class TreeSet<T extends Comparable<T>> implements Iterable<T>, Comparable<TreeSet<T>> {
-	private TreeNode<T> root;
-	private int size;
+    private TreeNode<T> root;
+    private int size;
 
-	public TreeSet() {
-		this.root = null;
-		this.size = 0;
+    public TreeSet() {
+	this.root = null;
+	this.size = 0;
+    }
+
+    public void add(T t) {
+	if (root == null) {
+	    this.root = new TreeNode<>(t);
+	    this.size++;
+	} else {
+	    this.root = add(root, t);
 	}
+    }
 
-	public void add(T t) {
-		if (root == null) {
-			this.root = new TreeNode<>(t);
-			this.size++;
+    private TreeNode<T> add(TreeNode<T> current, T t) {
+	if (current != null) {
+	    if (current.field.compareTo(t) > 0) {
+		current.left = add(current.left, t);
+	    } else if (current.field.compareTo(t) < 0) {
+		current.right = add(current.right, t);
+	    }
+	} else {
+	    this.size++;
+	    return new TreeNode<T>(t);
+	}
+	return current;
+    }
+
+    public void remove(T t) {
+	this.root = remove(root, t);
+    }
+
+    private TreeNode<T> remove(TreeNode<T> current, T t) {
+	if (current != null) {
+	    if (current.field.compareTo(t) == 0) {
+		if (current.right == null) {
+		    return current.left;
+		} else if (current.left == null) {
+		    return current.right;
 		} else {
-			this.root = add(root, t);
+		    TreeNode<T> minimum = minimumNode(current.right);
+		    minimum.right = remove(current.right, minimum.field);
+		    minimum.left = current.left;
+		    return minimum;
 		}
+	    } else if (current.field.compareTo(t) > 0) {
+		current.left = remove(current.left, t);
+	    } else {
+		current.right = remove(current.right, t);
+	    }
 	}
+	return current;
+    }
 
-	private TreeNode<T> add(TreeNode<T> current, T t) {
-		if (current != null) {
-			if (current.field.compareTo(t) > 0) {
-				current.left = add(current.left, t);
-			} else if (current.field.compareTo(t) < 0) {
-				current.right = add(current.right, t);
-			}
-		} else {
-			this.size++;
-			return new TreeNode<T>(t);
-		}
+    public boolean contains(T t) {
+	return this.contains(root, t);
+    }
+
+    private boolean contains(TreeNode<T> current, T t) {
+	if (current != null) {
+	    if (current.field.compareTo(t) > 0) {
+		return contains(current.left, t);
+	    } else if (current.field.compareTo(t) < 0) {
+		return contains(current.right, t);
+	    } else {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public int size() {
+	return this.size;
+    }
+
+    public T minimum() {
+	return this.minimumNode().field;
+    }
+
+    public T maximum() {
+	return this.maximumNode().field;
+    }
+
+    private TreeNode<T> minimumNode() {
+	return this.minimumNode(root);
+    }
+
+    private TreeNode<T> minimumNode(TreeNode<T> current) {
+	if (current != null) {
+	    if (current.left == null || current.isLeaf()) {
 		return current;
+	    }
+	    return minimumNode(current.left);
 	}
+	return null;
+    }
 
-	public void remove(T t) {
-		this.root = remove(root, t);
-	}
+    private TreeNode<T> maximumNode() {
+	return maximumNode(root);
+    }
 
-	private TreeNode<T> remove(TreeNode<T> current, T t) {
-		if (current != null) {
-			if (current.field.compareTo(t) == 0) {
-				if (current.right == null) {
-					return current.left;
-				} else if (current.left == null) {
-					return current.right;
-				} else {
-					TreeNode<T> minimum = minimumNode(current.right);
-					minimum.right = remove(current.right, minimum.field);
-					minimum.left = current.left;
-					return minimum;
-				}
-			} else if (current.field.compareTo(t) > 0) {
-				current.left = remove(current.left, t);
-			} else {
-				current.right = remove(current.right, t);
-			}
-		}
+    private TreeNode<T> maximumNode(TreeNode<T> current) {
+	if (current != null) {
+	    if (current.right == null || current.isLeaf()) {
 		return current;
+	    }
+	    return maximumNode(current.right);
 	}
+	return null;
+    }
 
-	public boolean contains(T t) {
-		return this.contains(root, t);
-	}
-	
-	private boolean contains(TreeNode<T> current, T t) {
-		if (current != null) {
-			if (current.field.compareTo(t) > 0) {
-				return contains(current.left, t);
-			} else if (current.field.compareTo(t) < 0) {
-				return contains(current.right, t);
-			} else {
-				return true;
-			}
-		}
-		return false;
-	}
+    public boolean isEmpty() {
+	return this.size == 0;
+    }
 
-	public int size() {
-		return this.size;
-	}
+    public List<T> toArrayList() {
+	return toArrayList(root, new ArrayList<T>());
+    }
 
-	public T minimum() {
-		return this.minimumNode().field;
+    private List<T> toArrayList(TreeNode<T> current, List<T> values) {
+	if (current != null) {
+	    toArrayList(current.left, values);
+	    values.add(current.field);
+	    toArrayList(current.right, values);
 	}
+	return values;
+    }
 
-	public T maximum() {
-		return this.maximumNode().field;
-	}
+    @Override
+    public String toString() {
+	return this.toArrayList().toString();
+    }
 
-	private TreeNode<T> minimumNode() {
-		return this.minimumNode(root);
-	}
+    @Override
+    public Iterator<T> iterator() {
+	return new TreeSetIterator();
+    }
 
-	private TreeNode<T> minimumNode(TreeNode<T> current) {
-		if (current != null) {
-			if (current.left == null || current.isLeaf()) {
-				return current;
-			}
-			return minimumNode(current.left);
-		}
-		return null;
-	}
+    @Override
+    public int compareTo(TreeSet<T> other) {
+	return this.size() - other.size();
+    }
 
-	private TreeNode<T> maximumNode() {
-		return maximumNode(root);
-	}
+    private class TreeSetIterator implements Iterator<T> {
+	private Stack<TreeNode<T>> storedNodes = new Stack<>();
+	private TreeNode<T> next;
 
-	private TreeNode<T> maximumNode(TreeNode<T> current) {
-		if (current != null) {
-			if (current.right == null || current.isLeaf()) {
-				return current;
-			}
-			return maximumNode(current.right);
-		}
-		return null;
-	}
-
-	public boolean isEmpty() {
-		return this.size == 0;
-	}
-
-	public List<T> toArrayList() {
-		return toArrayList(root, new ArrayList<T>());
-	}
-
-	private List<T> toArrayList(TreeNode<T> current, List<T> values) {
-		if (current != null) {
-			toArrayList(current.left, values);
-			values.add(current.field);
-			toArrayList(current.right, values);
-		}
-		return values;
+	private TreeSetIterator() {
+	    TreeNode<T> temp = TreeSet.this.root;
+	    while (temp != null) {
+		this.storedNodes.add(temp);
+		temp = temp.left;
+	    }
+	    this.next = storedNodes.peek();
 	}
 
 	@Override
-	public String toString() {
-		return this.toArrayList().toString();
+	public boolean hasNext() {
+	    return !storedNodes.isEmpty();
 	}
 
 	@Override
-	public Iterator<T> iterator() {
-		return new TreeSetIterator();
+	public T next() {
+	    if (!this.hasNext()) {
+		throw new IllegalStateException();
+	    }
+	    TreeNode<T> node = storedNodes.pop();
+	    if (node.right != null) {
+		TreeNode<T> currentRight = node.right;
+		TreeNode<T> currentLeft = currentRight.left;
+		this.storedNodes.add(currentRight);
+		while (currentLeft != null) {
+		    this.storedNodes.add(currentLeft);
+		    currentLeft = currentLeft.left;
+		}
+	    }
+	    this.next = node;
+	    return node.field;
 	}
 
 	@Override
-	public int compareTo(TreeSet<T> other) {
-		return this.size() - other.size();
+	public void remove() {
+	    if (this.next == null) {
+		throw new IllegalStateException();
+	    }
+	    TreeSet.this.remove(next.field);
 	}
-
-	private class TreeSetIterator implements Iterator<T> {
-		private Stack<TreeNode<T>> storedNodes = new Stack<>();
-		private TreeNode<T> next;
-
-		private TreeSetIterator() {
-			TreeNode<T> temp = TreeSet.this.root;
-			while (temp != null) {
-				this.storedNodes.add(temp);
-				temp = temp.left;
-			}
-			this.next = storedNodes.peek();
-		}
-
-		@Override
-		public boolean hasNext() {
-			return !storedNodes.isEmpty();
-		}
-
-		@Override
-		public T next() {
-			if (!this.hasNext()) {
-				throw new IllegalStateException();
-			}
-			TreeNode<T> node = storedNodes.pop();
-			if (node.right != null) {
-				TreeNode<T> currentRight = node.right;
-				TreeNode<T> currentLeft = currentRight.left;
-				this.storedNodes.add(currentRight);
-				while (currentLeft != null) {
-					this.storedNodes.add(currentLeft);
-					currentLeft = currentLeft.left;
-				}
-			}
-			this.next = node;
-			return node.field;
-		}
-
-		@Override
-		public void remove() {
-			if (this.next == null) {
-				throw new IllegalStateException();
-			}
-			TreeSet.this.remove(next.field);
-		}
-	}
+    }
 }
-
